0fd7eb47b19c857eeb3100eae844d68f49f58832
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-02-11 16:08:45 -0500

    add card banner components

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+export default () => {
+  return (
+    <a className="card-container" href="http://https://www.reactjs.org">
+      <CardBanner />
+      <CardContent />
+    </a>
+  );
+};
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+export default () => {
+  return (
+    <div className="card-content">
+      <h3>Get started with React</h3>
+      <p>React makes it painless to core interactive UIs. Design simple views for each state in your application</p>
+      <p className="greyed">reactjs.org</p>
+    </div>
+  );
+}
