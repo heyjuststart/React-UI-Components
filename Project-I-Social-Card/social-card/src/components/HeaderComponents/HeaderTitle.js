fa357e7f5ea66fc6fd6a36c0a33f49f301e8e3cb
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-02-11 16:09:16 -0500

    add card header components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+export default () => {
+  return (
+    <div className="header-title">
+      <p>
+        Lambda School <span>@LambdaSchool • 26 jan</span>
+      </p>
+    </div>
+  );
+}
