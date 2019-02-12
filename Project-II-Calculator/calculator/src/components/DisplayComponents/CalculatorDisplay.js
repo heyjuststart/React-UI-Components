911ea9a4809ea1dd113ccfad7e24d2032de0303b
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-02-12 16:41:26 -0500

    add display and style

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,7 @@
 import React from 'react';
 import './Display.css';
+
+export default (props) => {
+  const { value } = props;
+  return <div className="calculator-display">{ value !== undefined || 0 }</div>;
+};
