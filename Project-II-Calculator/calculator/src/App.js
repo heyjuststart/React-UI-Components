acca66e1812f59fe0fbe66ef0ab4e9a10b3558d9
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-02-12 16:40:14 -0500

    add html to app.js

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="calculator">
+      <CalculatorDisplay />
+      <div className="calculator-row">
+        <ActionButton className="flex3 clear-button">clear</ActionButton>
+        <ActionButton className="red">&divide;</ActionButton>
+      </div>
+      <div className="calculator-row">
+        <NumberButton>7</NumberButton>
+        <NumberButton>8</NumberButton>
+        <NumberButton>9</NumberButton>
+        <ActionButton className="red">&times;</ActionButton>
+      </div>
+      <div className="calculator-row">
+        <NumberButton>4</NumberButton>
+        <NumberButton>5</NumberButton>
+        <NumberButton>6</NumberButton>
+        <ActionButton className="red">&minus;</ActionButton>
+      </div>
+      <div className="calculator-row">
+        <NumberButton>1</NumberButton>
+        <NumberButton>2</NumberButton>
+        <NumberButton>3</NumberButton>
+        <ActionButton className="red">+</ActionButton>
+      </div>
+      <div className="calculator-row">
+        <NumberButton className="flex3">0</NumberButton>
+        <ActionButton className="red">=</ActionButton>
+      </div>
     </div>
   );
 };
